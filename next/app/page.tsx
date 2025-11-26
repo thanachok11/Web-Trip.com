e09ca699a736006d2a6a1296a851a713e2382557
ธนาโชค สุวรรณ์ <thanachok.s@kkumail.com>
2025-11-26 13:03:43 +0700

    Update page.tsx

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useMemo } from "react";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://web-trip-com.onrender.com/";
 
 interface ReviewItem {
   name: string;
